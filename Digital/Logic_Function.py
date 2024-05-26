63532ffd781fef39d523f8599828758ec394116f
Adam Ghatta <adam.ghatta@gmail.com>
2024-05-26 13:55:43 -0700

    edited files

--- Digital/Logic_Function.py
+++ Digital/Logic_Function.py
@@ -1,17 +1,21 @@
 import itertools
 
-def Y(x):  # Logic Function
+def Y(x,n):  # Logic Function
 
-    
-    A = int(x[0])
-    B = int(x[1])
-    C = int(x[2])
-    D = int(x[3])
-    return (A | (~B))&((~C)&D)|A
+    if n == len(x):
+        A = int(x[0])
+        B = int(x[1])
+        C = int(x[2])
+        D = int(x[3])
+        return (A | (~B))&((~C)&D)|A
+    else:
+        print("Number of inputs in logic function exceeds stated inputs")
+        exit()
 
 # Construct 4-bit truth table with boolean inputs
 
 n = int(input("Number of inputs: "))
+
 array = []
 arr = [[0]*2]*2**n
 
@@ -21,7 +25,7 @@ for i in range(2**n):
 
 
 for i in range(len(arr)):
-    arr[i][1]=Y(arr[i][0])
+    arr[i][1]=Y(arr[i][0],n)
 print("Truth Table:  \n")
 for i in range(2**n):
     print(str(arr[i][0]) + "   " + str(arr[i][1]))
